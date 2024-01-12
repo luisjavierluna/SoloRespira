3ba51956e4c36193d181ac04163fefee93ef77ce
luisjavierluna <javierlunaluis@gmail.com>
2024-01-11 20:06:52 -0600

    Add spring boot initial project files

---
+++ SoloRespira/src/main/java/com/SoloRespira/SoloRespira/SoloRespiraApplication.java
@@ -0,0 +1,13 @@
+package com.SoloRespira.SoloRespira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloRespiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloRespiraApplication.class, args);
+	}
+
+}
---
+++ SoloRespira/src/test/java/com/SoloRespira/SoloRespira/SoloRespiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoloRespira.SoloRespira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloRespiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
